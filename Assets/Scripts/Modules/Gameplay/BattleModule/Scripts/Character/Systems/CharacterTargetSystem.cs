bfce9464d57fbd747044a40f0b323f8c5f0578c1
smirnov.vit <79731843+vik-white@users.noreply.github.com>
2026-04-09 13:28:05 +0300

    fix

--- Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/Character/Systems/CharacterTargetSystem.cs
+++ Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/Character/Systems/CharacterTargetSystem.cs
@@ -9,64 +9,16 @@ namespace vikwhite.ECS
     [UpdateInGroup(typeof(GameplaySystemGroup))]
     public partial struct CharacterTargetSystem : ISystem
     {
-        /*private EntityQuery _characterQuery;
+        private EntityQuery _characterQuery;
 
         public void OnCreate(ref SystemState state)
         {
             _characterQuery = state.GetEntityQuery(ComponentType.ReadOnly<Character>(), ComponentType.ReadOnly<LocalTransform>());
-        }*/
+        }
         
         public void OnUpdate(ref SystemState state) {
-            var ecb = new EntityCommandBuffer(Allocator.Temp);
-            var allEntities = new NativeList<Entity>(Allocator.Temp);
-            var allPositions = new NativeList<float3>(Allocator.Temp);
-            
-            foreach (var (transform, entity) in SystemAPI.Query<RefRO<LocalTransform>>().WithAll<Character>().WithEntityAccess()) 
-            {
-                allEntities.Add(entity);
-                allPositions.Add(transform.ValueRO.Position);
-            }
-            
-            foreach (var (transform, entity) in SystemAPI.Query<RefRO<LocalTransform>>().WithAll<Character>().WithEntityAccess()) 
-            {
-                float3 myPosition = transform.ValueRO.Position;
-                float minDistance = float.MaxValue;
-                Entity nearest = Entity.Null;
-                
-                for (int i = 0; i < allEntities.Length; i++) 
-                {
-                    if (allEntities[i] == entity) continue;
-                    if (SystemAPI.HasComponent<Enemy>(allEntities[i]) == SystemAPI.HasComponent<Enemy>(entity)) continue;
-                    float distance = math.distance(myPosition, allPositions[i]);
-                    if (distance < minDistance) 
-                    {
-                        minDistance = distance;
-                        nearest = allEntities[i];
-                    }
-                }
-
-                if (nearest != Entity.Null)
-                {
-                    if (!SystemAPI.HasComponent<Target>(entity))
-                        ecb.AddComponent(entity, new Target { Value = nearest });
-                    else
-                        ecb.SetComponent(entity, new Target { Value = nearest });
-                }
-                else
-                {
-                    if (SystemAPI.HasComponent<Target>(entity))
-                        ecb.RemoveComponent<Target>(entity);
-                }
-            }
-            
-            allEntities.Dispose();
-            allPositions.Dispose();
-            ecb.Playback(state.EntityManager);
-            ecb.Dispose();
-            
-            
-            /*var ecb = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(state.WorldUnmanaged).AsParallelWriter();
             var characters = _characterQuery.ToEntityArray(Allocator.TempJob);
+            var ecb = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(state.WorldUnmanaged).AsParallelWriter();
             var job = new CharacterTargetJob
             {
                 Characters = characters,
@@ -75,9 +27,8 @@ namespace vikwhite.ECS
                 TargetLookup = SystemAPI.GetComponentLookup<Target>(true),
                 Ecb = ecb
             };
-            state.Dependency = job.Schedule(state.Dependency);
-            state.Dependency.Complete();
-            characters.Dispose(state.Dependency);*/
+            state.Dependency = job.ScheduleParallel(state.Dependency);
+            characters.Dispose(state.Dependency);
         }
     }
     
--- Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/LifeTime/System/DestroyOutsideSceneSystem.cs
+++ Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/LifeTime/System/DestroyOutsideSceneSystem.cs
@@ -10,14 +10,12 @@ namespace vikwhite
     public partial struct DestroyOutsideSceneSystem : ISystem
     {
         public void OnUpdate(ref SystemState state) {
-            var ecb = new EntityCommandBuffer(Unity.Collections.Allocator.Temp);
+            var ecb = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(state.WorldUnmanaged);
             foreach (var (transform, entity) in SystemAPI.Query<RefRO<LocalTransform>>().WithAny<DestroyOutsideScene>().WithEntityAccess()) {
                 var position = transform.ValueRO.Position;
                 if (position.x > 20 || position.x < -20 || position.y > 50 || position.y < -1 || position.z > 20 || position.z < -20) 
                     ecb.DestroyEntity(entity);
             }
-            ecb.Playback(state.EntityManager);
-            ecb.Dispose();
         }
     }
 }
