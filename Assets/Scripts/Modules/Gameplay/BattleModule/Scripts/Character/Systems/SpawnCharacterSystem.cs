47ea07195360c3bebd3145c51fcaeb3c1a12a318
smirnov.vit <79731843+vik-white@users.noreply.github.com>
2026-04-10 16:48:57 +0300

    fix

--- Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/Character/Systems/SpawnCharacterSystem.cs
+++ Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/Character/Systems/SpawnCharacterSystem.cs
@@ -26,9 +26,8 @@ namespace vikwhite.ECS
             ecb.CreateFrameEntity(new CreateCharacter{Config = sceletonBoss, IsEnemy = true, Position = new float3(Random.Range(5f, 10f), 0, Random.Range(-5f, 5f))});
             
             var sceleton = SystemAPI.GetSingletonBuffer<CharacterConfig>()[1];
-            for (int i = 0; i < 50; i++) ecb.CreateFrameEntity(new CreateCharacter{Config = sceleton, IsEnemy = true, Position = new float3(Random.Range(-5f, 5f), 0, Random.Range(5f, 10f))});
-            for (int i = 0; i < 50; i++) ecb.CreateFrameEntity(new CreateCharacter{Config = sceleton, IsEnemy = true, Position = new float3(Random.Range(-5f, -10f), 0, Random.Range(-5f, 5f))});
-            for (int i = 0; i < 50; i++) ecb.CreateFrameEntity(new CreateCharacter{Config = sceleton, IsEnemy = true, Position = new float3(Random.Range(5f, 10f), 0, Random.Range(-5f, 5f))});
+            for (int i = 0; i < 80; i++) ecb.CreateFrameEntity(new CreateCharacter{Config = sceleton, IsEnemy = true, Position = new float3(Random.Range(-5f, -10f), 0, Random.Range(-5f, 5f))});
+            for (int i = 0; i < 80; i++) ecb.CreateFrameEntity(new CreateCharacter{Config = sceleton, IsEnemy = true, Position = new float3(Random.Range(5f, 10f), 0, Random.Range(-5f, 5f))});
             
             ecb.Playback(state.EntityManager);
             state.Enabled = false;
