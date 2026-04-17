d735c30349fababce69969eec8668d54c460eac6
smirnov.vit <79731843+vik-white@users.noreply.github.com>
2026-04-17 14:47:49 +0300

    fix

--- Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/Aura/Systems/CreateAuraSystem.cs
+++ Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/Aura/Systems/CreateAuraSystem.cs
@@ -14,18 +14,19 @@ namespace vikwhite
             var ecb = new EntityCommandBuffer(Unity.Collections.Allocator.Temp);
             foreach (var request in SystemAPI.Query<RefRO<CreateAura>>()) {
                 var ability = request.ValueRO.Ability;
-                var aura = ecb.Instantiate(SystemAPI.GetSingletonBuffer<Prefab>()[ability.Prefab].Value);
+                var aura = ability.Prefab != -1 ?  ecb.Instantiate(SystemAPI.GetSingletonBuffer<Prefab>()[ability.Prefab].Value) : ecb.CreateEntity();
                 ecb.AddComponent<SceneEntity>(aura);
                 ecb.AddComponent(aura, new Aura
                 {
                     Interval = ability.AuraInterval,
                     TimeLeft = ability.AuraLifetime
                 });
-                ecb.SetComponent(aura, new LocalTransform {
-                    Position = SystemAPI.GetComponent<LocalTransform>(request.ValueRO.Provider).Position,
-                    Rotation = quaternion.identity,
-                    Scale = ability.AuraRadius
-                });
+                if(ability.Prefab != -1)
+                    ecb.SetComponent(aura, new LocalTransform {
+                        Position = SystemAPI.GetComponent<LocalTransform>(request.ValueRO.Provider).Position,
+                        Rotation = quaternion.identity,
+                        Scale = ability.AuraRadius
+                    });
                 ecb.AddComponent(aura, new Provider{ Value = request.ValueRO.Provider });
                 ecb.AddComponent(aura, new Effects{ Array = ability.Effects, Ability = new AbilityLevelData{ ID = ability.ID, Level = ability.Level } });
                 ecb.AddComponent(aura, new Statuses{ Array = ability.Statuses, Ability = new AbilityLevelData{ ID = ability.ID, Level = ability.Level } });
