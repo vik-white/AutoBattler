6ef43b5e01b053a919e9d9d510c45556c35d0bf2
smirnov.vit <79731843+vik-white@users.noreply.github.com>
2026-04-23 14:39:39 +0300

    Clase All Windows

--- Assets/Scripts/Modules/Core/EnvironmentModule/Scripts/EnvironmentStateMachine.cs
+++ Assets/Scripts/Modules/Core/EnvironmentModule/Scripts/EnvironmentStateMachine.cs
@@ -8,15 +8,18 @@ namespace vikwhite
     public class EnvironmentStateMachine : IEnvironmentStateMachine
     {
         private readonly IEnvironmentStateFactory _factory;
+        private readonly IWindowManager _windowManager;
         private IEnvironmentState _currentState;
 
-        public EnvironmentStateMachine(IEnvironmentStateFactory factory)
+        public EnvironmentStateMachine(IEnvironmentStateFactory factory, IWindowManager windowManager)
         {
             _factory = factory;
+            _windowManager = windowManager;
         }
 
         public void SwitchState(EnvironmentType key)
         {
+            _windowManager.CloseAllWindows();
             _currentState?.Exit();
             _currentState = _factory.Get(key);
             _currentState.Enter();
--- Assets/Scripts/Modules/Core/WindowModule/Sctipts/WindowManager.cs
+++ Assets/Scripts/Modules/Core/WindowModule/Sctipts/WindowManager.cs
@@ -1,21 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
 namespace vikwhite
 {
     public interface IWindowManager
     {
         void ShowWindow(IWindowView view);
         void CloseWindow(IWindowView view);
+        void CloseAllWindows();
     }
     
     public class WindowManager : IWindowManager
     {
+        private readonly HashSet<IWindowView> _openedWindows = new();
+
         public void ShowWindow(IWindowView view)
         {
+            _openedWindows.Add(view);
             view.ShowInternal();
         }
 
         public void CloseWindow(IWindowView view)
         {
             view.CloseInternal();
+            _openedWindows.Remove(view);
+        }
+
+        public void CloseAllWindows()
+        {
+            Debug.Log("CloseAllWindows");
+            foreach (var view in _openedWindows.ToList())
+            {
+                if (view.ViewModel != null)
+                    view.ViewModel.Close();
+                else
+                    CloseWindow(view);
+            }
         }
     }
 }
