f42d733980f21c053ba57acee43ec936dd11bfd9
smirnov.vit <79731843+vik-white@users.noreply.github.com>
2026-04-25 12:57:56 +0300

    AbilitySystem fix

--- Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/Ability/Systems/AbilitySystem.cs
+++ Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/Ability/Systems/AbilitySystem.cs
@@ -13,102 +13,77 @@ namespace vikwhite.ECS
             var ecb = new EntityCommandBuffer(Unity.Collections.Allocator.Temp);
             foreach (var (abilities, transform, character, entity) in SystemAPI.Query<DynamicBuffer<Ability>, RefRO<LocalTransform>, RefRO<Character>>().WithEntityAccess())
             {
+                bool hasTarget = SystemAPI.HasComponent<Target>(entity);
+                uint activeAbility = SystemAPI.HasComponent<ActiveAbility>(entity) ? SystemAPI.GetComponent<ActiveAbility>(entity).Value : 0;
+                bool useActiveAbility = SystemAPI.HasComponent<UseAbility>(entity);
+                var statBuffer = SystemAPI.GetBuffer<StatMultiply>(entity);
+                float activeCooldownMultiply = statBuffer[(int)StatType.ActiveAbilityCooldownMultiply].Value;
+                float cooldownMultiply = statBuffer[(int)StatType.CooldownMultiply].Value;
+                var characterConfig = character.ValueRO.GetConfig();
+                Entity target = hasTarget ? SystemAPI.GetComponent<Target>(entity).Value : Entity.Null;
+                var targetTransform = hasTarget ? SystemAPI.GetComponent<LocalTransform>(target) : default;
+                var targetConfig = hasTarget ? SystemAPI.GetComponent<Character>(target).GetConfig() : default;
+
                 for (int i = 0; i < abilities.Length; i++)
                 {
                     ref var ability = ref abilities.ElementAt(i);
                     var abilityConfig = ability.GetConfig();
                     ability.IsActivate = false;
 
-                    if (ability.IsChild) continue;
-                    if (!SystemAPI.HasComponent<Target>(entity)) continue;
+                    if (ability.IsChild || !hasTarget) continue;
 
-                    var activeAbility = SystemAPI.HasComponent<ActiveAbility>(entity) ? SystemAPI.GetComponent<ActiveAbility>(entity).Value : 0;
-                    var statBuffer = SystemAPI.GetBuffer<StatMultiply>(entity);
-                    var cooldownMultiply = activeAbility == abilityConfig.ID ? statBuffer[(int)StatType.ActiveAbilityCooldownMultiply].Value : statBuffer[(int)StatType.CooldownMultiply].Value;
-                    ability.Cooldown += dt * cooldownMultiply;
+                    ability.Cooldown += dt * (activeAbility == abilityConfig.ID ? activeCooldownMultiply : cooldownMultiply);
+                    if (ability.Cooldown <= abilityConfig.Cooldown) continue;
 
-                    if (ability.Cooldown > abilityConfig.Cooldown)
+                    bool isActiveAbility = abilityConfig.ID == activeAbility;
+                    if (isActiveAbility)
                     {
-                        if (abilityConfig.ID != activeAbility)
-                        {
-                            var distance = float.MaxValue;
-                            var target = SystemAPI.GetComponent<Target>(entity).Value;
-                            var characterConfig = character.ValueRO.GetConfig();
-                            var targetConfig = SystemAPI.GetComponent<Character>(target).GetConfig();
-                            var baseDistance = abilityConfig.Radius + characterConfig.ColliderRadius + targetConfig.ColliderRadius;
-                            if (SystemAPI.HasComponent<Target>(entity))
-                            {
-                                var targetTransform = SystemAPI.GetComponent<LocalTransform>(target);
-                                var direction = targetTransform.Position - transform.ValueRO.Position;
-                                distance = math.length(direction);
-                            }
-                            if (distance <= baseDistance || abilityConfig.Radius == 0)
-                            {
-                                ability.Cooldown = 0;
-
-                                if (abilityConfig.Type != AbilityType.Abilities)
-                                {
-                                    ability.IsAnimation = true;
-                                    var speedMultiply = SystemAPI.GetBuffer<StatMultiply>(entity)[(int)StatType.CooldownMultiply].Value;
-                                    ecb.CreateFrameEntity(new Animation { Character = entity, Type = abilityConfig.Animation, Speed = speedMultiply });
-                                    if (abilityConfig.CastVFXPrefab != 0) ecb.CreateFrameEntity(new CreatePrefabEvent { ID = abilityConfig.CastVFXPrefab, Position = transform.ValueRO.Position });
-                                }
-                                else
-                                {
-                                    for (int j = 0; j < abilities.Length; j++)
-                                    {
-                                        ref var abilityChild = ref abilities.ElementAt(j);
-                                        if (abilityChild.IsChild)
-                                        {
-                                            var abilityChildConfig = abilityChild.GetConfig();
-                                            abilityChild.IsActivate = false;
-                                            abilityChild.IsAnimation = true;
-                                            var speedMultiply = SystemAPI.GetBuffer<StatMultiply>(entity)[(int)StatType.CooldownMultiply].Value;
-                                            ecb.CreateFrameEntity(new Animation { Character = entity, Type = abilityChildConfig.Animation, Speed = speedMultiply });
-                                            if (abilityChildConfig.CastVFXPrefab != 0) ecb.CreateFrameEntity(new CreatePrefabEvent { ID = abilityChildConfig.CastVFXPrefab, Position = transform.ValueRO.Position });
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        else
-                        {
-                            var useAbility = SystemAPI.HasComponent<UseAbility>(entity) ? activeAbility : 0;
-                            if (abilityConfig.ID == useAbility)
-                            {
-                                ecb.RemoveComponent<UseAbility>(entity);
-                                ability.Cooldown = 0;
-
-                                if (abilityConfig.CastVFXPrefab != 0) ecb.CreateFrameEntity(new CreatePrefabEvent { ID = abilityConfig.CastVFXPrefab, Position = transform.ValueRO.Position });
-
-                                if (abilityConfig.Type != AbilityType.Abilities)
-                                {
-                                    ability.IsAnimation = true;
-                                    var speedMultiply = SystemAPI.GetBuffer<StatMultiply>(entity)[(int)StatType.CooldownMultiply].Value;
-                                    ecb.CreateFrameEntity(new Animation { Character = entity, Type = abilityConfig.Animation, Speed = speedMultiply });
-                                }
-                                else
-                                {
-                                    for (int j = 0; j < abilities.Length; j++)
-                                    {
-                                        ref var abilityChild = ref abilities.ElementAt(j);
-                                        if (abilityChild.IsChild)
-                                        {
-                                            var abilityChildConfig = abilityChild.GetConfig();
-                                            abilityChild.IsActivate = false;
-                                            abilityChild.IsAnimation = true;
-                                            var speedMultiply = SystemAPI.GetBuffer<StatMultiply>(entity)[(int)StatType.CooldownMultiply].Value;
-                                            ecb.CreateFrameEntity(new Animation { Character = entity, Type = abilityChildConfig.Animation, Speed = speedMultiply });
-                                            if (abilityChildConfig.CastVFXPrefab != 0) ecb.CreateFrameEntity(new CreatePrefabEvent { ID = abilityChildConfig.CastVFXPrefab, Position = transform.ValueRO.Position });
-                                        }
-                                    }
-                                }
-                            }
-                        }
+                        if (!useActiveAbility) continue;
+                        ecb.RemoveComponent<UseAbility>(entity);
                     }
+                    else if (!CanUseOnTarget(transform.ValueRO, targetTransform, abilityConfig, characterConfig, targetConfig)) continue;
+
+                    ability.Cooldown = 0;
+                    TriggerAbility(ecb, abilities, entity, transform.ValueRO.Position, abilityConfig, cooldownMultiply, ref ability);
                 }
             }
             ecb.Playback(state.EntityManager);
+        }
+
+        private static bool CanUseOnTarget(in LocalTransform transform, in LocalTransform targetTransform, in AbilityConfig abilityConfig, in CharacterConfigData characterConfig, in CharacterConfigData targetConfig)
+        {
+            if (abilityConfig.Radius == 0) return true;
+
+            var distance = math.distance(transform.Position, targetTransform.Position);
+            var maxDistance = abilityConfig.Radius + characterConfig.ColliderRadius + targetConfig.ColliderRadius;
+            return distance <= maxDistance;
+        }
+
+        private static void TriggerAbility(EntityCommandBuffer ecb, DynamicBuffer<Ability> abilities, Entity entity, float3 position, in AbilityConfig abilityConfig, float speedMultiply, ref Ability ability)
+        {
+            if (abilityConfig.Type != AbilityType.Abilities)
+            {
+                ability.IsAnimation = true;
+                PlayAbility(ecb, entity, position, abilityConfig, speedMultiply);
+                return;
+            }
+
+            for (int i = 0; i < abilities.Length; i++)
+            {
+                ref var childAbility = ref abilities.ElementAt(i);
+                if (!childAbility.IsChild) continue;
+
+                var childConfig = childAbility.GetConfig();
+                childAbility.IsActivate = false;
+                childAbility.IsAnimation = true;
+                PlayAbility(ecb, entity, position, childConfig, speedMultiply);
+            }
+        }
+
+        private static void PlayAbility(EntityCommandBuffer ecb, Entity entity, float3 position, in AbilityConfig abilityConfig, float speedMultiply)
+        {
+            ecb.CreateFrameEntity(new Animation { Character = entity, Type = abilityConfig.Animation, Speed = speedMultiply });
+            if (abilityConfig.CastVFXPrefab != 0) ecb.CreateFrameEntity(new CreatePrefabEvent { ID = abilityConfig.CastVFXPrefab, Position = position });
         }
     }
 }
