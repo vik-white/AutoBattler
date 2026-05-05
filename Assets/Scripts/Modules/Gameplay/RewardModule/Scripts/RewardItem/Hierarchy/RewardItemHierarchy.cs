fc36db27beb19108fdab1b6576710a2145a2992b
smirnov.vit <79731843+vik-white@users.noreply.github.com>
2026-05-05 16:20:05 +0300

    RewardItem

---
+++ Assets/Scripts/Modules/Gameplay/RewardModule/Scripts/RewardItem/Hierarchy/RewardItemHierarchy.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace vikwhite
+{
+    public class RewardItemHierarchy : MonoBehaviour
+    {
+        public TMP_Text Amount;
+        public Image Icon;
+    }
+}
