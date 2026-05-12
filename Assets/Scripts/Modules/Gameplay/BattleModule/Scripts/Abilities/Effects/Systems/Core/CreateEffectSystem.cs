739487f68ade3c28990605dd6b103dea0d29a908
smirnov.vit <79731843+vik-white@users.noreply.github.com>
2026-05-12 20:13:44 +0300

    GetSkillMultiplier

--- Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/Abilities/Effects/Systems/Core/CreateEffectSystem.cs
+++ Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/Abilities/Effects/Systems/Core/CreateEffectSystem.cs
@@ -15,7 +15,8 @@ namespace vikwhite.ECS
             foreach (var request in SystemAPI.Query<RefRO<CreateEffect>>()) {
                 var type = request.ValueRO.Data.Type;
                 var provider = request.ValueRO.Provider;
-                var value = GetEffectValue(ref state, levelUpConfigs, request.ValueRO.Data, provider);
+                var abilityID = request.ValueRO.Ability.IsCreated ? request.ValueRO.Ability.Value.ID : 0;
+                var value = GetEffectValue(ref state, levelUpConfigs, request.ValueRO.Data, provider, abilityID);
                 var isCrit = false;
                 if (type == EffectType.Damage)
                     value = TryApplyCrit(ref characters, ref critCounters, provider, value, out isCrit);
@@ -39,7 +40,7 @@ namespace vikwhite.ECS
             ecb.Playback(state.EntityManager);
         }
 
-        public float GetEffectValue(ref SystemState state, BlobAssetReference<BlobArrayContainer<LevelUpConfig>> levelUpConfigs, EffectData effect, Entity entity)
+        public float GetEffectValue(ref SystemState state, BlobAssetReference<BlobArrayContainer<LevelUpConfig>> levelUpConfigs, EffectData effect, Entity entity, uint abilityID)
         {
             var character = SystemAPI.GetComponent<Character>(entity);
             var config = character.GetConfig();
@@ -50,21 +51,36 @@ namespace vikwhite.ECS
             var level = character.Level - 1;
             var stars = character.Stars;
             var skillLevel = character.SkillLevel - 1;
-            
+
+            var skillMultiplier = GetSkillMultiplier(config, abilityID, level, stars, skillLevel, levelUpConfig, starsLevelUpConfig, skillLevelUpConfig);
+            var effectValue = effect.Value * skillMultiplier;
+
             var value = effect.Dependence switch
             {
-                EffectDependenceType.Attack => (config.Attack * GetLevelUpMultiply(level, stars, skillLevel, levelUpConfig.Attack, starsLevelUpConfig.Attack, skillLevelUpConfig.Attack)) * effect.Value,
-                EffectDependenceType.Defense => (config.Defense * GetLevelUpMultiply(level, stars, skillLevel, levelUpConfig.Defense, starsLevelUpConfig.Defense, skillLevelUpConfig.Defense)) * effect.Value,
-                EffectDependenceType.Health => (config.Health * GetLevelUpMultiply(level, stars, skillLevel, levelUpConfig.Health, starsLevelUpConfig.Health, skillLevelUpConfig.Health)) * effect.Value,
-                EffectDependenceType.CritChance => (config.CritChance * GetLevelUpMultiply(level, stars, skillLevel, levelUpConfig.CritChance, starsLevelUpConfig.CritChance, skillLevelUpConfig.CritChance)) * effect.Value,
-                EffectDependenceType.CritValue => (config.CritValue * GetLevelUpMultiply(level, stars, skillLevel, levelUpConfig.CritValue, starsLevelUpConfig.CritValue, skillLevelUpConfig.CritValue)) * effect.Value,
-                _ => effect.Value,
+                EffectDependenceType.Attack => (config.Attack * GetLevelUpMultiply(level, stars, skillLevel, levelUpConfig.Attack, starsLevelUpConfig.Attack, skillLevelUpConfig.Attack)) * effectValue,
+                EffectDependenceType.Defense => (config.Defense * GetLevelUpMultiply(level, stars, skillLevel, levelUpConfig.Defense, starsLevelUpConfig.Defense, skillLevelUpConfig.Defense)) * effectValue,
+                EffectDependenceType.Health => (config.Health * GetLevelUpMultiply(level, stars, skillLevel, levelUpConfig.Health, starsLevelUpConfig.Health, skillLevelUpConfig.Health)) * effectValue,
+                EffectDependenceType.CritChance => (config.CritChance * GetLevelUpMultiply(level, stars, skillLevel, levelUpConfig.CritChance, starsLevelUpConfig.CritChance, skillLevelUpConfig.CritChance)) * effectValue,
+                EffectDependenceType.CritValue => (config.CritValue * GetLevelUpMultiply(level, stars, skillLevel, levelUpConfig.CritValue, starsLevelUpConfig.CritValue, skillLevelUpConfig.CritValue)) * effectValue,
+                _ => effectValue,
             };
 
             if (effect.Type == EffectType.Damage)
                 value *= SystemAPI.GetBuffer<StatMultiply>(entity)[(int)StatType.DamageMultiply].Value;
 
             return value;
+        }
+
+        private float GetSkillMultiplier(in CharacterConfigData config, uint abilityID, int level, int stars, int skillLevel, in LevelUpConfig levelUpConfig, in LevelUpConfig starsLevelUpConfig, in LevelUpConfig skillLevelUpConfig)
+        {
+            if (abilityID == 0) return 1f;
+            if (abilityID == config.SkillActive) return GetLevelUpMultiply(level, stars, skillLevel, levelUpConfig.SkillActive, starsLevelUpConfig.SkillActive, skillLevelUpConfig.SkillActive);
+            if (abilityID == config.SkillPassive1) return GetLevelUpMultiply(level, stars, skillLevel, levelUpConfig.SkillPassive1, starsLevelUpConfig.SkillPassive1, skillLevelUpConfig.SkillPassive1);
+            if (abilityID == config.SkillPassive2) return GetLevelUpMultiply(level, stars, skillLevel, levelUpConfig.SkillPassive2, starsLevelUpConfig.SkillPassive2, skillLevelUpConfig.SkillPassive2);
+            if (abilityID == config.SkillMeta1) return GetLevelUpMultiply(level, stars, skillLevel, levelUpConfig.SkillMeta1, starsLevelUpConfig.SkillMeta1, skillLevelUpConfig.SkillMeta1);
+            if (abilityID == config.SkillMeta2) return GetLevelUpMultiply(level, stars, skillLevel, levelUpConfig.SkillMeta2, starsLevelUpConfig.SkillMeta2, skillLevelUpConfig.SkillMeta2);
+            if (abilityID == config.SkillMeta3) return GetLevelUpMultiply(level, stars, skillLevel, levelUpConfig.SkillMeta3, starsLevelUpConfig.SkillMeta3, skillLevelUpConfig.SkillMeta3);
+            return 1f;
         }
 
         private float GetLevelUpMultiply(int levelIndex, int starsIndex, int skillIndex, float levelMultiply, float starsMultiply, float skillMultiply)
