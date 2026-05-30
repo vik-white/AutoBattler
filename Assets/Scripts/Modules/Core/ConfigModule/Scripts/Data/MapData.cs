8f0b8bed732e188a650bf08e85abd9caee7bc961
smirnov.vit <79731843+vik-white@users.noreply.github.com>
2026-05-30 17:17:00 +0300

    fix

--- Assets/Scripts/Modules/Core/ConfigModule/Scripts/Data/MapData.cs
+++ Assets/Scripts/Modules/Core/ConfigModule/Scripts/Data/MapData.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace vikwhite.Data
 {
@@ -7,17 +9,26 @@ namespace vikwhite.Data
         string ID { get; }
         LocationType Type { get; }
         string Sector { get; }
+        GameObject Prefab { get; }
     }
     
     [Serializable]
-    public class MapData : IMapData
+    public class MapData : IMapData, ICustomJsonParser
     {
         public string ID;
         public LocationType Type;
         public string Sector;
+        public GameObject Prefab;
         
         string IMapData.ID => ID;
         LocationType IMapData.Type => Type;
         string IMapData.Sector => Sector;
+        GameObject IMapData.Prefab => Prefab;
+        
+        public void Parse(Dictionary<string, string> row)
+        {
+            var prefabName = row["Prefab"];
+            if (prefabName != "") Prefab = Resources.Load<GameObject>($"Characters/Prefabs/{prefabName}/{prefabName}");
+        }
     }
 }
