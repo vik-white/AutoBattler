5eca1e665550190cd2b6a69767d20669c3087b1b
smirnov.vit <79731843+vik-white@users.noreply.github.com>
2026-05-31 19:30:59 +0300

    fix

--- Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/UI/BattleWindow/View/BattleWindowView.cs
+++ Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/UI/BattleWindow/View/BattleWindowView.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UniRx;
 using UnityEngine;
 
@@ -8,6 +9,7 @@ namespace vikwhite
         private readonly IBattleSkillViewFactory _skillViewFactory;
         private readonly IBattleHealthBarViewFactory _healthBarViewFactory;
         private readonly IBattleDamageFlyTextViewFactory _damageFlyTextViewFactory;
+        private readonly List<IView> _createdViews = new();
 
         public BattleWindowView(GameObject view, IBattleSkillViewFactory skillViewFactory, IBattleHealthBarViewFactory healthBarViewFactory, IBattleDamageFlyTextViewFactory damageFlyTextViewFactory) : base(view)
         {
@@ -32,6 +34,12 @@ namespace vikwhite
             Register(Disposable.Create(() => viewModel.DamageFlyTextCreated -= CreateDamageFlyText));
         }
 
+        protected override void ReleaseViewModel()
+        {
+            DestroyCreatedViews();
+            base.ReleaseViewModel();
+        }
+
         private void UpdateFPS()
         {
             if (_view.FPS != null && BaseViewModel != null) _view.FPS.text = BaseViewModel.FpsText;
@@ -39,17 +47,33 @@ namespace vikwhite
 
         private void CreateSkill(BattleSkillViewModel skill)
         {
-            AddDisposable(_skillViewFactory.Get(skill, _view.SkillContainer));
+            AddCreatedView(_skillViewFactory.Get(skill, _view.SkillContainer));
         }
 
         private void CreateHealthBar(BattleHealthBarViewModel healthBar)
         {
-            AddDisposable(_healthBarViewFactory.Get(healthBar, _view.transform));
+            AddCreatedView(_healthBarViewFactory.Get(healthBar, _view.transform));
         }
 
         private void CreateDamageFlyText(BattleDamageFlyTextViewModel flyText)
         {
-            AddDisposable(_damageFlyTextViewFactory.Get(flyText, _view.transform));
+            AddCreatedView(_damageFlyTextViewFactory.Get(flyText, _view.transform));
+        }
+
+        private void AddCreatedView(IView view)
+        {
+            _createdViews.Add(view);
+        }
+
+        private void DestroyCreatedViews()
+        {
+            foreach (var view in _createdViews)
+            {
+                if (view?.GameObject == null) continue;
+                view.DisposeAndDestroy();
+            }
+
+            _createdViews.Clear();
         }
     }
 }
