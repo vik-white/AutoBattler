dbb2d10188fe834907ba0e16f1d5d26fe49dfb9c
smirnov.vit <79731843+vik-white@users.noreply.github.com>
2026-06-01 13:50:29 +0300

    fix

--- Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/UI/BattleWindow/View/BattleWindowView.cs
+++ Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/UI/BattleWindow/View/BattleWindowView.cs
@@ -9,7 +9,6 @@ namespace vikwhite
         private readonly IBattleSkillViewFactory _skillViewFactory;
         private readonly IBattleHealthBarViewFactory _healthBarViewFactory;
         private readonly IBattleDamageFlyTextViewFactory _damageFlyTextViewFactory;
-        private readonly List<IView> _createdViews = new();
 
         public BattleWindowView(GameObject view, IBattleSkillViewFactory skillViewFactory, IBattleHealthBarViewFactory healthBarViewFactory, IBattleDamageFlyTextViewFactory damageFlyTextViewFactory) : base(view)
         {
@@ -22,6 +21,8 @@ namespace vikwhite
         {
             BindClick(_view.QuickVictoryButton, viewModel.OnQuickVictory);
 
+            _view.SkillContainer.ClearChildren();
+            
             foreach (var healthBar in viewModel.HealthBars) CreateHealthBar(healthBar);
             foreach (var skill in viewModel.Skills) CreateSkill(skill);
 
@@ -34,12 +35,6 @@ namespace vikwhite
             Register(Disposable.Create(() => viewModel.DamageFlyTextCreated -= CreateDamageFlyText));
         }
 
-        protected override void ReleaseViewModel()
-        {
-            DestroyCreatedViews();
-            base.ReleaseViewModel();
-        }
-
         private void UpdateFPS()
         {
             if (_view.FPS != null && BaseViewModel != null) _view.FPS.text = BaseViewModel.FpsText;
@@ -47,33 +42,17 @@ namespace vikwhite
 
         private void CreateSkill(BattleSkillViewModel skill)
         {
-            AddCreatedView(_skillViewFactory.Get(skill, _view.SkillContainer));
+            _skillViewFactory.Get(skill, _view.SkillContainer);
         }
 
         private void CreateHealthBar(BattleHealthBarViewModel healthBar)
         {
-            AddCreatedView(_healthBarViewFactory.Get(healthBar, _view.transform));
+            _healthBarViewFactory.Get(healthBar, _view.transform);
         }
 
         private void CreateDamageFlyText(BattleDamageFlyTextViewModel flyText)
         {
-            AddCreatedView(_damageFlyTextViewFactory.Get(flyText, _view.transform));
-        }
-
-        private void AddCreatedView(IView view)
-        {
-            _createdViews.Add(view);
-        }
-
-        private void DestroyCreatedViews()
-        {
-            foreach (var view in _createdViews)
-            {
-                if (view?.GameObject == null) continue;
-                view.DisposeAndDestroy();
-            }
-
-            _createdViews.Clear();
+            _damageFlyTextViewFactory.Get(flyText, _view.transform);
         }
     }
 }
