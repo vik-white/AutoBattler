47d4b5d72a6b6180b59b21f6c06eb3bb87f05815
smirnov.vit <79731843+vik-white@users.noreply.github.com>
2026-06-22 20:45:43 +0300

    fix

--- Assets/Scripts/Modules/Gameplay/CharacterModule/Scripts/CharacterWindow/View/CharacterWindowView.cs
+++ Assets/Scripts/Modules/Gameplay/CharacterModule/Scripts/CharacterWindow/View/CharacterWindowView.cs
@@ -11,10 +11,13 @@ namespace vikwhite
             BindClick(_view.CloseButton, viewModel.Close);
             BindClick(_view.LevelUpButton, viewModel.OnUpgradeLevel);
             Bind(viewModel.Level, level => _view.Level.text = level.ToString());
+            Bind(viewModel.ExpResources.Amount, _ => SetLevelUpPrice());
             CreateView<StarsView, StarsHierarchy>(_view.Stars).Initialize(viewModel.Stars);
             _view.Name.text = viewModel.Name;
             _view.Image.sprite = viewModel.Image;
-            _view.LevelUpPrice.text = $"{viewModel.ExpResources.Amount}/{viewModel.LevelUpPrice}";
+            SetLevelUpPrice();
         }
+
+        private void SetLevelUpPrice() => _view.LevelUpPrice.text = $"{ViewModel.ExpResources.Amount}/{ViewModel.LevelUpPrice}";
     }
 }
