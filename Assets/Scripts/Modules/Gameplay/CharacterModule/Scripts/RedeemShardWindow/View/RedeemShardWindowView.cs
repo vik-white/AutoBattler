98b733be9085dd83c1f87d2462120f1a63c76553
smirnov.vit <79731843+vik-white@users.noreply.github.com>
2026-06-24 16:13:04 +0300

    fix

--- Assets/Scripts/Modules/Gameplay/CharacterModule/Scripts/RedeemShardWindow/View/RedeemShardWindowView.cs
+++ Assets/Scripts/Modules/Gameplay/CharacterModule/Scripts/RedeemShardWindow/View/RedeemShardWindowView.cs
@@ -26,6 +26,7 @@ namespace vikwhite
             _view.HeroShardIcon.sprite = viewModel.HeroShardIcon;
             _view.ShardIcon1.sprite = viewModel.ShardIcon;
             _view.ShardIcon2.sprite = viewModel.ShardIcon;
+            _view.RedeemButton.interactable = viewModel.Selected.Value > 0;
             _view.Slider.SetValue(viewModel.ShardsAmount.Value > 0 ? viewModel.Selected.Value / (float)viewModel.ShardsAmount.Value : 0f);
         }
     }
