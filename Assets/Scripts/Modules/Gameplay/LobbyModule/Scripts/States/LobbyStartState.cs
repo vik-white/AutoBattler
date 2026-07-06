4b5c5066b9956dfd9f5e7ebbb516b229fda10919
smirnov.vit <79731843+vik-white@users.noreply.github.com>
2026-07-06 17:23:16 +0300

    Tavern scene

--- Assets/Scripts/Modules/Gameplay/LobbyModule/Scripts/States/LobbyStartState.cs
+++ Assets/Scripts/Modules/Gameplay/LobbyModule/Scripts/States/LobbyStartState.cs
@@ -10,15 +10,18 @@ namespace vikwhite
     public class LobbyStartState : ILobbyStartState
     {
         private readonly ILobbyWindow _lobbyWindow;
+        private readonly ICameraService _camera;
         
-        public LobbyStartState(ILobbyWindow lobbyWindow)
+        public LobbyStartState(ILobbyWindow lobbyWindow, ICameraService camera)
         {
             _lobbyWindow = lobbyWindow;
+            _camera = camera;
         }
         
         public void Enter() 
         {
             _lobbyWindow.ShowWindow();
+            _camera.Initialize(new Vector3(-61.28f, 58.9f, -35.26f), Quaternion.Euler(39.456f, 60.041f, 0.26f), 10);
         }
 
         public void Exit()
