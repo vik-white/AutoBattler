6a0a565127d795953be40a94c23a0bd997a8c075
smirnov.vit <79731843+vik-white@users.noreply.github.com>
2026-07-14 15:02:30 +0300

    fix

--- Assets/Scripts/Modules/Gameplay/LobbyModule/Scripts/Rooms/RoomsService.cs
+++ Assets/Scripts/Modules/Gameplay/LobbyModule/Scripts/Rooms/RoomsService.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using vikwhite.Data;
 
 namespace vikwhite
 {
@@ -11,24 +12,27 @@ namespace vikwhite
     public class RoomsService : IRoomsService
     {
         private readonly IProfileService _profile;
+        private readonly IConfigs _configs;
         private List<Room> _rooms = new();
         
-        public RoomsService(IProfileService profile)
+        public RoomsService(IProfileService profile, IConfigs configs)
         {
             _profile = profile;
+            _configs = configs;
         }
 
         public void Initialize()
         {
             var tavern = UnityEngine.Object.FindAnyObjectByType<TavernHierarchy>(FindObjectsInactive.Include);
-
             foreach (var room in tavern.Rooms)
             {
                 room.Container.ClearChildren();
+                var profileData = _profile.Data.Rooms.Find(e => e.Type == room.Type);
+                var configData = _configs.Rooms.GetAll().Find(e => e.Type == room.Type);
+                _rooms.Add(new Room(profileData.Type, profileData.Level));
+                var roomGO = GameObject.Instantiate(configData.Prefab, room.Container);
+                roomGO.transform.localPosition = Vector3.zero;
             }
-            
-            //foreach (var roomData in _profile.Data.Rooms)
-            //    _rooms.Add(new Room(roomData.Type, roomData.Level));
         }
     }
 }
