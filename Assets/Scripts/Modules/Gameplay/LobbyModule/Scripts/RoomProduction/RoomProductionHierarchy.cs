737f254b0f5a4f541cb5cddd668567c89dd8e3ce
smirnov.vit <79731843+vik-white@users.noreply.github.com>
2026-07-15 15:27:39 +0300

    RoomProduction

---
+++ Assets/Scripts/Modules/Gameplay/LobbyModule/Scripts/RoomProduction/RoomProductionHierarchy.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomProductionHierarchy : MonoBehaviour
+{
+    public Image Icon;
+    public TMP_Text Value;
+    public TMP_Text Time;
+}
