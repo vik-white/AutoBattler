875c987db622ba5133cd04a3e4e22f98c77c1465
smirnov.vit <79731843+vik-white@users.noreply.github.com>
2026-07-30 08:41:56 +0300

    BreakthroughHierarchy

---
+++ Assets/Scripts/Modules/Gameplay/CharacterModule/Scripts/BreakthroughWindow/Hierarchy/BreakthroughHierarchy.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace vikwhite
+{
+    public class BreakthroughHierarchy : MonoBehaviour
+    {
+        public TMP_Text NextLevel;
+        public TMP_Text CurrentLevel;
+        public Button BreakthroughButton;
+        public Button CloseButton;
+        public Button CloseFadeButton;
+        
+        public TMP_Text AttackCurrent;
+        public TMP_Text AttackAdd;
+        public TMP_Text DefenseCurrent;
+        public TMP_Text DefenseAdd;
+        public TMP_Text HealthCurrent;
+        public TMP_Text HealthAdd;
+        
+        public TMP_Text EssenceCount;
+        public TMP_Text ExpCount;
+        public TMP_Text HeroesCount;
+    }
+}
