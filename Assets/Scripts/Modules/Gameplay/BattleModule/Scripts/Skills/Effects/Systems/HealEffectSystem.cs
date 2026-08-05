45723ed0a301c1eda509d82b35a5f83887af0290
smirnov.vit <79731843+vik-white@users.noreply.github.com>
2026-08-05 11:38:47 +0300

    fix

--- Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/Skills/Effects/Systems/HealEffectSystem.cs
+++ Assets/Scripts/Modules/Gameplay/BattleModule/Scripts/Skills/Effects/Systems/HealEffectSystem.cs
@@ -8,12 +8,12 @@ namespace vikwhite.ECS
     {
         public void OnUpdate(ref SystemState state) {
             var healths = SystemAPI.GetComponentLookup<Health>();
+            var healthMaxes = SystemAPI.GetComponentLookup<HealthMax>(true);
             foreach (var (effect, target) in SystemAPI.Query<RefRO<Effect>, RefRO<Target>>().WithAny<HealEffect>())
             {
                 var character = target.ValueRO.Value;
-                var config = SystemAPI.GetComponent<Character>(character).GetConfig();
                 var health = healths[character].Value + effect.ValueRO.Value;
-                if (health > config.Health) health = config.Health;
+                if (health > healthMaxes[character].Value) health = healthMaxes[character].Value;
                 healths[character] = new Health { Value = health };
             }
         }
